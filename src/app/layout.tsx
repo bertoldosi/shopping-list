e81f4002d395a0bd92c6419d761b5436ff01b2ff
bertoldosi <matheus.bertoldosi@gmail.com>
2023-11-24 14:16:13 -0300

    feat: Ajustando comportamento dos componentes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="pt_BR">
       <body
+        suppressHydrationWarning={true}
         style={{
           background: "#444444",
           overflow: "hidden",
